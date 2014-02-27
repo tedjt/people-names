a59cf1f014444058724a58bd529413ccf56deddc
Ted Tomlinson <zumbino@gmail.com>
2014-02-27 00:32:13 -0800

    improve matching

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ module.exports.looseCompare = function validateName(source, input) {
   var sFirstName = firstClean(input);
   var iFirstName = firstClean(input);
   var iLastName = lastClean(input);
+  console.log('comparing names: %s %s %s %s', sFirstName, sLastName, iFirstName, iLastName);
+  if (sFirstName === iFirstName && iLastName === sLastName) return true;
   if (sLastName.length < 2 && sFirstName === iFirstName && iLastName[0] === sLastName[0]) return true;
   return false;
 };
